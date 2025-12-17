c933d180c457c8408ca47276301e5ab87799d060
Vinicius Dacal <viniciusldacal@gmail.com>
2025-12-17 19:29:46 -0300

    feat: fix avatar styles

--- packages/react/src/components/ui/avatar.tsx
+++ packages/react/src/components/ui/avatar.tsx
@@ -7,7 +7,7 @@ function Avatar({ className, ...props }: React.ComponentProps<typeof AvatarPrimi
   return (
     <AvatarPrimitive.Root
       data-slot="avatar"
-      className={cn('relative flex size-8 shrink-0 overflow-hidden rounded-full', className)}
+      className={cn(className, 'relative flex shrink-0 overflow-hidden rounded-full', 'size-8')}
       {...props}
     />
   );
